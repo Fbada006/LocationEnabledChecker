63bea1ec3c9c4cb9aea233c2dd6a087dbd7ab304
Fbada006 <ferdinandkamata@yahoo.com>
2020-11-23 17:55:57 +0300

    Add code to check if location services are on

--- app/src/main/java/com/example/locationenabledchecker/MainActivity.kt
+++ app/src/main/java/com/example/locationenabledchecker/MainActivity.kt
@@ -1,11 +1,66 @@
 package com.example.locationenabledchecker
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.IntentSender
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationSettingsRequest
 
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_CHECK_SETTINGS = 43234
+    private val TAG = "MainActivity"
+    private val locationRequest = LocationRequest.create()?.apply {
+        interval = 10000
+        fastestInterval = 5000
+        priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val builder = locationRequest?.let {
+            LocationSettingsRequest.Builder()
+                .addLocationRequest(it)
+        }
+
+        val client = LocationServices.getSettingsClient(this)
+        val task = client.checkLocationSettings(builder?.build())
+
+        task.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException) {
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    exception.startResolutionForResult(
+                        this@MainActivity,
+                        REQUEST_CHECK_SETTINGS
+                    )
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CHECK_SETTINGS) {
+            if (resultCode == RESULT_OK ){
+                Log.d(
+                    TAG,
+                    "onActivityResult: -----------------Location permissions have been satisfied"
+                )
+            } else {
+                Log.d(
+                    TAG,
+                    "onActivityResult: -----------------Location services off"
+                )
+            }
+        }
     }
 }
